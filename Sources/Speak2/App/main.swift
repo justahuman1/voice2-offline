1ae924ae47cd70b58b81db500380ad0eff9b93d8
Sai Valla <saiv@linkedin.com>
2026-03-29 14:21:16 -0700

    fix: retain AppDelegate to prevent menu bar icon from disappearing

--- Sources/Speak2/App/main.swift
+++ Sources/Speak2/App/main.swift
@@ -1,9 +1,12 @@
 import AppKit
 
+nonisolated(unsafe) var appDelegate: AppDelegate?
+
 @MainActor
 func bootstrap() {
     let app = NSApplication.shared
     let delegate = AppDelegate()
+    appDelegate = delegate
     app.delegate = delegate
     app.setActivationPolicy(.accessory)
     app.run()
